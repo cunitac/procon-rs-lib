23995ebd046cbac4ba6c665b9906f911c430ceb1
cunitac <cunitac@gmail.com>
2020-09-25 19:56:26 +0900

    refactor

--- src/seg_tree.rs
+++ src/seg_tree.rs
@@ -3,32 +3,6 @@ use std::{
     ops::{Bound, Range, RangeBounds},
 };
 
-pub trait Element: Sized + Clone + Debug {}
-impl<T: Sized + Clone + Debug> Element for T {}
-
-pub trait Monoid {
-    type Item: Element;
-    fn id() -> Self::Item;
-    fn op(a: &Self::Item, b: &Self::Item) -> Self::Item;
-    fn fold<'a, I>(iterable: I) -> Self::Item
-    where
-        I: IntoIterator<Item = &'a Self::Item>,
-        Self::Item: 'a,
-    {
-        iterable
-            .into_iter()
-            .fold(Self::id(), |a, b| Self::op(&a, b))
-    }
-}
-
-pub enum AddI64 {}
-
-impl Monoid for AddI64 {
-    type Item = i64;
-    fn id() -> i64 { 0 }
-    fn op(a: &i64, b: &i64) -> i64 { a + b }
-}
-
 /// 便利な列 `st`
 pub struct SegTree<M: Monoid> {
     len: usize,
@@ -38,7 +12,9 @@ pub struct SegTree<M: Monoid> {
 
 impl<M: Monoid> SegTree<M> {
     /// `st = [M::id(); n]`
-    pub fn new(n: usize) -> Self { Self::from(&vec![M::id(); n][..]) }
+    pub fn new(n: usize) -> Self {
+        Self::from(&vec![M::id(); n][..])
+    }
     /// `st[i] = v`
     pub fn update(&mut self, i: usize, v: M::Item) {
         assert!(i < self.len, "index out: {}/{}", i, self.len);
@@ -55,13 +31,15 @@ impl<M: Monoid> SegTree<M> {
         self.val = M::op(&left.val, &right.val);
     }
     /// `st[i]`
-    pub fn get(&self, i: usize) -> M::Item { self.fold(i ..= i) }
+    pub fn get(&self, i: usize) -> M::Item {
+        self.fold(i..=i)
+    }
     /// `st[range].fold(M::id(), |a, b| M::op(&a, &b))`
     pub fn fold(&self, range: impl RangeBounds<usize>) -> M::Item {
         let Range { start, end } = range_from(range, self.len);
-        self.inner_fold(start, end)
+        self.fold_inner(start, end)
     }
-    fn inner_fold(&self, start: usize, end: usize) -> M::Item {
+    fn fold_inner(&self, start: usize, end: usize) -> M::Item {
         let len = end - start;
         if len == 0 {
             return M::id();
@@ -70,21 +48,31 @@ impl<M: Monoid> SegTree<M> {
         }
         let mid = self.len / 2;
         let (left, right) = self.child.as_ref().unwrap().as_ref();
-        M::op(
-            &left.fold(start.min(mid) .. end.min(mid)),
-            &right.fold(start.max(mid) - mid .. end.max(mid) - mid),
-        )
+        if end <= mid {
+            left.fold_inner(start, end)
+        } else if mid <= start {
+            right.fold_inner(start - mid, end - mid)
+        } else {
+            M::op(
+                &left.fold_inner(start, mid),
+                &right.fold_inner(0, end - mid),
+            )
+        }
     }
     /// `pred(st.fold(start..end))` なる最大の `end`
     /// `pred(M::id())` が要請される
     pub fn max_end<P>(&self, start: usize, mut pred: P) -> usize
-    where P: FnMut(&M::Item) -> bool {
+    where
+        P: FnMut(&M::Item) -> bool,
+    {
         assert!(start <= self.len, "index out: {}/{}", start, self.len);
         let mut acc = M::id();
-        self.inner_max_end(start, &mut pred, &mut acc)
+        self.max_end_inner(start, &mut pred, &mut acc)
     }
-    fn inner_max_end<P>(&self, start: usize, pred: &mut P, acc: &mut M::Item) -> usize
-    where P: FnMut(&M::Item) -> bool {
+    fn max_end_inner<P>(&self, start: usize, pred: &mut P, acc: &mut M::Item) -> usize
+    where
+        P: FnMut(&M::Item) -> bool,
+    {
         if start == 0 {
             let merged = M::op(acc, &self.val);
             if pred(&merged) {
@@ -93,29 +81,36 @@ impl<M: Monoid> SegTree<M> {
             } else if self.len == 1 {
                 return 0;
             }
-        } else if self.len == 1 {
-            return 1;
+        } else if start == self.len {
+            return self.len;
         }
         let mid = self.len / 2;
         let (left, right) = self.child.as_ref().unwrap().as_ref();
         if start < mid {
-            let res_left = left.inner_max_end(start, pred, acc);
+            let res_left = left.max_end_inner(start, pred, acc);
             if res_left < mid {
-                return res_left;
+                res_left
+            } else {
+                mid + right.max_end_inner(0, pred, acc)
             }
+        } else {
+            mid + right.max_end_inner(start - mid, pred, acc)
         }
-        mid + right.inner_max_end(start.max(mid) - mid, pred, acc)
     }
     /// `pred(st.fold(start..end))` なる最小の `start`
     /// `pred(M::id())` が要請される
     pub fn min_start<P>(&self, end: usize, mut pred: P) -> usize
-    where P: FnMut(&M::Item) -> bool {
+    where
+        P: FnMut(&M::Item) -> bool,
+    {
         assert!(end <= self.len, "index out: {}/{}", end, self.len);
         let mut acc = M::id();
-        self.inner_min_start(end, &mut pred, &mut acc)
+        self.min_start_inner(end, &mut pred, &mut acc)
     }
-    fn inner_min_start<P>(&self, end: usize, pred: &mut P, acc: &mut M::Item) -> usize
-    where P: FnMut(&M::Item) -> bool {
+    fn min_start_inner<P>(&self, end: usize, pred: &mut P, acc: &mut M::Item) -> usize
+    where
+        P: FnMut(&M::Item) -> bool,
+    {
         if end == self.len {
             let merged = M::op(acc, &self.val);
             if pred(&merged) {
@@ -124,18 +119,21 @@ impl<M: Monoid> SegTree<M> {
             } else if self.len == 1 {
                 return 1;
             }
-        } else if self.len == 1 {
+        } else if end == 0 {
             return 0;
         }
         let mid = self.len / 2;
         let (left, right) = self.child.as_ref().unwrap().as_ref();
         if mid <= end {
-            let res_right = right.inner_min_start(end - mid, pred, acc);
+            let res_right = right.min_start_inner(end - mid, pred, acc);
             if res_right > 0 {
-                return mid + res_right;
+                mid + res_right
+            } else {
+                left.min_start_inner(mid, pred, acc)
             }
+        } else {
+            left.min_start_inner(end, pred, acc)
         }
-        left.inner_min_start(end.min(mid), pred, acc)
     }
 }
 
@@ -167,34 +165,63 @@ impl<M: Monoid> From<&[M::Item]> for SegTree<M> {
             }
         } else {
             let mid = slice.len() / 2;
-            let ch1 = Self::from(&slice[.. mid]);
-            let ch2 = Self::from(&slice[mid ..]);
+            let left = Self::from(&slice[..mid]);
+            let right = Self::from(&slice[mid..]);
             Self {
                 len: slice.len(),
-                val: M::op(&ch1.val, &ch2.val),
-                child: Some(Box::new((ch1, ch2))),
+                val: M::op(&left.val, &right.val),
+                child: Some(Box::new((left, right))),
             }
         }
     }
 }
 
+pub trait Element: Sized + Clone + Debug {}
+impl<T: Sized + Clone + Debug> Element for T {}
+
+pub trait Monoid {
+    type Item: Element;
+    fn id() -> Self::Item;
+    fn op(a: &Self::Item, b: &Self::Item) -> Self::Item;
+    fn fold<'a, I>(iterable: I) -> Self::Item
+    where
+        I: IntoIterator<Item = &'a Self::Item>,
+        Self::Item: 'a,
+    {
+        iterable
+            .into_iter()
+            .fold(Self::id(), |a, b| Self::op(&a, b))
+    }
+}
+
+#[macro_export]
+macro_rules! monoid {
+    (type $t:ident = ($item:ty, $op:expr, $id:expr)) => {
+        enum $t {}
+        impl Monoid for $t {
+            type Item = $item;
+            fn op(a: &$item, b: &$item) -> $item {
+                $op(a, b)
+            }
+            fn id() -> $item {
+                $id
+            }
+        }
+    };
+}
+
 #[test]
 fn test_seg_tree() {
-    pub enum M {}
-    impl Monoid for M {
-        type Item = i32;
-        fn id() -> i32 { 0 }
-        fn op(a: &i32, b: &i32) -> i32 { a + b }
-    }
+    monoid!(type M = (i32, |a, b| a + b, 0));
     let sq = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     let st = SegTree::<M>::from(&sq[..]);
-    for i in 0 .. sq.len() {
-        for j in i .. sq.len() {
-            assert_eq!(sq[i .. j].iter().sum::<i32>(), st.fold(i .. j))
+    for i in 0..sq.len() {
+        for j in i..sq.len() {
+            assert_eq!(sq[i..j].iter().sum::<i32>(), st.fold(i..j))
         }
     }
-    for start in 0 ..= sq.len() {
-        for max in 0 ..= 55 {
+    for start in 0..=sq.len() {
+        for max in 0..=55 {
             let mut acc = 0;
             let mut right = start;
             while right < sq.len() && acc + sq[right] <= max {
@@ -204,8 +231,8 @@ fn test_seg_tree() {
             assert_eq!(st.max_end(start, |&sum| sum <= max), right);
         }
     }
-    for end in 0 ..= sq.len() {
-        for max in 0 ..= 55 {
+    for end in 0..=sq.len() {
+        for max in 0..=55 {
             let mut acc = 0;
             let mut left = end;
             while left > 0 && acc + sq[left - 1] <= max {
