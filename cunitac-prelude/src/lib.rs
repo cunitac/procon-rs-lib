bf279369c534c8c323168146192b1163cce499b8
cunitac <cunitac@gmail.com>
2021-01-01 21:23:57 +0900

    prelude

---
+++ cunitac-prelude/src/lib.rs
@@ -0,0 +1,8 @@
+pub use {
+    easyio::{stdio, Bytes, Chars, Concat, Lines, Usize1, Words},
+    std::{
+        cmp::{self, Ordering, Reverse},
+        collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
+        iter::FromIterator,
+    },
+};
