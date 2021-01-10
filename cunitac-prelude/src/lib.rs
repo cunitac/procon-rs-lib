a6a86178f4970c2a9241ad8265ae3baf0a83e476
cunitac <cunitac@gmail.com>
2021-01-10 23:19:40 +0900

    vecn, vecln

--- cunitac-prelude/src/lib.rs
+++ cunitac-prelude/src/lib.rs
@@ -1,5 +1,5 @@
 pub use {
-    easyio::{stdio, Bytes, Chars, Concat, Lines, Usize1, Words},
+    easyio::{stdio, Bytes, Chars, Concat, Lines, Usize1, Words, VecLn, VecN},
     std::{
         cmp::{self, Ordering, Reverse},
         collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
