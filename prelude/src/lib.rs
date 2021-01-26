192b2f8a0de2d394782a63c7bb6f4825dcf48385
cunitac <cunitac@gmail.com>
2021-01-26 23:40:57 +0900

    a

--- prelude/src/lib.rs
+++ prelude/src/lib.rs
@@ -3,6 +3,7 @@ pub use {
     std::{
         cmp::{self, Ordering, Reverse},
         collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
+        io::{Read, Write},
         iter::FromIterator,
     },
 };
