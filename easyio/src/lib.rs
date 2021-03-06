1d559bdd668442cc91e125a753602882e3c5645a
cunitac <cunitac@gmail.com>
2021-03-06 12:51:45 +0900

    a

--- easyio/src/lib.rs
+++ easyio/src/lib.rs
@@ -2,5 +2,10 @@ pub mod i;
 pub mod o;
 
 pub mod prelude {
-    pub use super::{i::marker::*, o::marker::*};
+    pub use super::{
+        i::{marker::*, Source},
+        o::{marker::*, Printer},
+    };
+    use std::io::{StdinLock, StdoutLock};
+    pub type Stdio<'a> = (Source<StdinLock<'a>>, Printer<StdoutLock<'a>>);
 }
