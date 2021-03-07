35c521114301f06d64c94204e25a8e7b0159d6cd
cunitac <cunitac@gmail.com>
2021-03-08 01:29:30 +0900

    a

---
+++ capture-fn/src/lib.rs
@@ -0,0 +1,98 @@
+#[macro_export]
+macro_rules! capture {
+    (
+        #[capture($($ca:tt)*)]
+        fn $name:ident($($arg:tt)*) -> $ret:ty $body:block
+    ) => {
+        capture_inner!([][$($ca)*,] fn $name($($arg)*) -> $ret $body)
+    };
+}
+
+#[macro_export]
+macro_rules! capture_inner {
+    (
+        [$(($g:ident, $ga:expr, $gt:ty))*][]
+        fn $name:ident($($a:ident: $at:ty),*) -> $ret:ty $body:block
+    ) => {
+        fn $name($($g: $gt,)* $($a: $at,)*) -> $ret {
+            #[allow(unused_macros)]
+            macro_rules! $name {
+                () => {
+                    |$($a),*| $name($($g,)* $($a,)*)
+                }
+            }
+            $body
+        }
+        #[allow(unused_mut)]
+        let mut $name = |$($a),*| $name($($ga,)* $($a,)*);
+    };
+    ([$($g:tt)*][]fn $name:ident($($a:ident: $at:ty),*,) $($rest:tt)*) => {
+        capture_inner!([$($g)*][]fn $name($($a: $at),*) $($rest)*)
+    };
+    ([$($done:tt)*][,] $($info:tt)*) => {
+        capture_inner!([$($done)*][] $($info)*)
+    };
+    ([$($done:tt)*][$g:ident: &mut $gt:ty, $($rest:tt)*] $($info:tt)*) => {
+        capture_inner!([$($done)* ($g, &mut $g, &mut $gt)][$($rest)*] $($info)*)
+    };
+    ([$($done:tt)*][$g:ident: &$gt:ty, $($rest:tt)*] $($info:tt)*) => {
+        capture_inner!([$($done)* ($g, &$g, &$gt)][$($rest)*] $($info)*)
+    };
+    ([$($done:tt)*][$g:ident: $gt:ty, $($rest:tt)*] $($info:tt)*) => {
+        capture_inner!([$($done)* ($g, $g, $gt)][$($rest)*]$($info)*)
+    };
+}
+
+#[macro_export]
+macro_rules! memoise {
+    (
+        #[capture($($ca:tt)*)]
+        fn $name:ident($($arg:tt)*) -> $ret:ty $body:block
+    ) => {
+        memoise_inner!([][$($ca)*,] fn $name($($arg)*) -> $ret $body)
+    };
+    (fn $name:ident($($arg:tt)*) -> $ret:ty $body:block) => {
+        memoise_inner!([][] fn $name($($arg)*) -> $ret $body)
+    };
+}
+
+#[macro_export]
+macro_rules! memoise_inner {
+    (
+        [$(($g:ident, $ga:expr, $gt:ty))*][]
+        fn $name:ident($($a:ident: $at:ty),*) -> $ret:ty $body:block
+    ) => {
+        type Memo = ::std::collections::HashMap<($($at,)*), $ret>;
+        fn $name(memo: &mut Memo, $($g: $gt,)* $($a: $at,)*) -> $ret {
+            #[allow(unused_macros)]
+            macro_rules! $name {
+                () => {
+                    |$($a),*| {
+                        if let ::std::option::Option::Some(ret) = memo.get(&($($a,)*)) {
+                            ret.clone()
+                        } else {
+                            let ret = $name(memo, $($g,)* $($a,)*);
+                            memo.insert(($($a,)*), ret.clone());
+                            ret
+                        }
+                    }
+                }
+            }
+            $body
+        }
+        let mut memo = Memo::new();
+        let mut $name = |$($a),*| $name(&mut memo, $($ga,)* $($a,)*);
+    };
+    ([$($g:tt)*][]fn $name:ident($($a:ident: $at:ty),*,) $($rest:tt)*) => {
+        memoise_inner!([$($g)*][]fn $name($($a: $at),*) $($rest)*)
+    };
+    ([$($done:tt)*][,] $($info:tt)*) => {
+        memoise_inner!([$($done)*][] $($info)*)
+    };
+    ([$($done:tt)*][$g:ident: &$gt:ty, $($rest:tt)*] $($info:tt)*) => {
+        memoise_inner!([$($done)* ($g, &$g, &$gt)][$($rest)*] $($info)*)
+    };
+    ([$($done:tt)*][$g:ident: $gt:ty, $($rest:tt)*] $($info:tt)*) => {
+        memoise_inner!([$($done)* ($g, $g, $gt)][$($rest)*]$($info)*)
+    };
+}
