d27d8761540474a779eaa379f6b7c804a4d51cd0
cunitac <cunitac@gmail.com>
2021-03-08 00:10:53 +0900

    a

--- prelude/src/lib.rs
+++ prelude/src/lib.rs
@@ -1,5 +1,5 @@
 pub use {
-    easyio::{stdio, Bytes, Chars, Concat, Lines, Usize1, VecLn, VecN, Words, IO},
+    easyio::prelude::*,
     std::{
         cmp::{self, Ordering, Reverse},
         collections::{BTreeMap, BTreeSet, HashMap, HashSet, VecDeque},
