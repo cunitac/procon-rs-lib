f1f61ea5467b88829fabc6e4582e5736198ccd85
cunitac <cunitac@gmail.com>
2021-03-15 14:51:02 +0900

    a

--- timer/src/lib.rs
+++ timer/src/lib.rs
@@ -7,30 +7,52 @@ pub struct Timer {
 }
 
 impl Timer {
-    pub fn new(millis: u64) -> Self {
+    pub fn from_millis(millis: u64) -> Self {
         Self {
             begin: Instant::now(),
             duration: Duration::from_millis(millis),
         }
     }
-    pub fn from_to(from: Instant, to: Instant) -> Self {
-        Self {
-            begin: from,
-            duration: to - from,
-        }
-    }
     /// `duration` を `1.0` として
-    pub fn now(&self) -> f64 {
+    pub fn elapsed(&self) -> f64 {
         (Instant::now() - self.begin).as_secs_f64() / self.duration.as_secs_f64()
     }
-    pub fn end(&self) -> bool {
-        Instant::now() >= self.begin + self.duration
+    pub fn is_over(&self) -> bool {
+        Instant::now() - self.begin > self.duration
     }
-    /// 現在から、「このタイマーの `begin` から `ration * duration` 後」までのタイマー
+    pub fn freeze(self) -> FreezedTimer {
+        FreezedTimer {
+            freezed: Instant::now(),
+            timer: self,
+        }
+    }
+    /// 現在から `duration * ratio` 後までの `Timer`
+    /// `self` 終了後も続くとパニック
     pub fn subtimer(&self, ratio: f64) -> Self {
-        let now = Instant::now();
-        assert!(ratio <= 1.0, "too long timer");
-        Self::from_to(now, self.begin + self.duration.mul_f64(ratio))
+        let begin = Instant::now();
+        let duration = self.duration.mul_f64(ratio);
+        assert!(
+            self.begin + self.duration >= begin + duration,
+            "too long subtimer, elapsed = {:.3}, ratio = {:.3}",
+            self.elapsed(),
+            ratio
+        );
+        Self { begin, duration }
+    }
+}
+
+#[derive(Clone, Copy)]
+pub struct FreezedTimer {
+    freezed: Instant,
+    timer: Timer,
+}
+
+impl FreezedTimer {
+    pub fn restart(self) -> Timer {
+        Timer {
+            begin: self.timer.begin + (Instant::now() - self.freezed),
+            duration: self.timer.duration,
+        }
     }
 }
 
