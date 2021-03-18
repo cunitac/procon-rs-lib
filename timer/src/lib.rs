07c9d230d2ada72b5ff0ac5c7e86949d281af813
cunitac <cunitac@gmail.com>
2021-03-19 02:11:49 +0900

    a

--- timer/src/lib.rs
+++ timer/src/lib.rs
@@ -7,18 +7,18 @@ pub struct Timer {
 }
 
 impl Timer {
-    pub fn from_millis(millis: u64) -> Self {
+    pub fn new(secs: f64) -> Self {
         Self {
             begin: Instant::now(),
-            duration: Duration::from_millis(millis),
+            duration: Duration::from_secs_f64(secs),
         }
     }
     /// `duration` を `1.0` として
     pub fn elapsed(&self) -> f64 {
-        (Instant::now() - self.begin).as_secs_f64() / self.duration.as_secs_f64()
+        self.elapsed_duration().as_secs_f64() / self.duration.as_secs_f64()
     }
     pub fn is_over(&self) -> bool {
-        Instant::now() - self.begin > self.duration
+        self.elapsed_duration() > self.duration
     }
     pub fn freeze(self) -> FreezedTimer {
         FreezedTimer {
@@ -26,18 +26,15 @@ impl Timer {
             timer: self,
         }
     }
-    /// 現在から `duration * ratio` 後までの `Timer`
-    /// `self` 終了後も続くとパニック
-    pub fn subtimer(&self, ratio: f64) -> Self {
-        let begin = Instant::now();
-        let duration = self.duration.mul_f64(ratio);
-        assert!(
-            self.begin + self.duration >= begin + duration,
-            "too long subtimer, elapsed = {:.3}, ratio = {:.3}",
-            self.elapsed(),
-            ratio
-        );
-        Self { begin, duration }
+    /// 現在から、「`begin` から `secs` 秒後」まで
+    pub fn subtimer(&self, secs: f64) -> Self {
+        Self {
+            begin: Instant::now(),
+            duration: Duration::from_secs_f64(secs) - self.elapsed_duration(),
+        }
+    }
+    fn elapsed_duration(&self) -> Duration {
+        Instant::now() - self.begin
     }
 }
 
