7a7823e61a490b8aa4a9f80d04ada31a8f0b4165
cunitac <cunitac@gmail.com>
2021-09-29 22:58:21 +0900

    a

---
+++ graph/rerooting/src/lib.rs
@@ -0,0 +1,55 @@
+pub fn rerooting_dp<T, F, G, H>(edge: &[(usize, usize)], id: T, f: F, g: G, h: H) -> Vec<T>
+where
+    T: Clone + Default,
+    F: Fn(&T, &T) -> T,
+    G: Fn(&T, usize) -> T,
+    H: Fn(&T, usize) -> T,
+{
+    let n = edge.len() + 1;
+    let mut adj = vec![vec![]; n];
+    let mut ix_for_adj = vec![vec![]; n];
+    for &(i, j) in edge {
+        ix_for_adj[i].push(adj[j].len());
+        ix_for_adj[j].push(adj[i].len());
+        adj[i].push(j);
+        adj[j].push(i);
+    }
+    let mut order = Vec::with_capacity(n);
+    let mut parent = vec![0; n];
+    let mut ix_for_parent = vec![0; n];
+    let mut stack = vec![0];
+    while let Some(i) = stack.pop() {
+        order.push(i);
+        for (ix, &j) in adj[i].iter().enumerate() {
+            if j != parent[i] {
+                stack.push(j);
+                parent[j] = i;
+                ix_for_parent[j] = ix;
+            }
+        }
+    }
+    let mut dp: Vec<_> = adj.iter().map(|a| vec![T::default(); a.len()]).collect();
+    for &i in order[1..].iter().rev() {
+        let mut prod = id.clone();
+        for (ix, &j) in adj[i].iter().enumerate().filter(|&(_, &j)| j != parent[i]) {
+            prod = f(&prod, &g(&dp[i][ix], j));
+        }
+        dp[parent[i]][ix_for_parent[i]] = h(&prod, parent[i]);
+    }
+    let mut ret = vec![T::default(); n];
+    for &i in order.iter() {
+        let mut accum_back = vec![T::default(); adj[i].len()];
+        *accum_back.last_mut().unwrap() = id.clone();
+        for j in (1..accum_back.len()).rev() {
+            accum_back[j - 1] = f(&g(&dp[i][j], adj[i][j]), &accum_back[j]);
+        }
+        let mut accum = id.clone();
+        for (ix, &j) in adj[i].iter().enumerate() {
+            let prod = f(&accum, &accum_back[ix]);
+            dp[j][ix_for_adj[i][ix]] = h(&prod, j);
+            accum = f(&accum, &g(&dp[i][ix], j));
+        }
+        ret[i] = h(&accum, i);
+    }
+    ret
+}
