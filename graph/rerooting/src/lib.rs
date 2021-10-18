6de7661385a2cdae540c4dfaf47b2f633c616e90
cunitac <cunitac@gmail.com>
2021-10-18 21:49:24 +0900

    U

--- graph/rerooting/src/lib.rs
+++ graph/rerooting/src/lib.rs
@@ -3,12 +3,13 @@
 /// - c[i][j] = i を根とした j の子
 /// - (T, f, id) は可換モノイド
 /// としたとき、ret[i] = dp[i][i] なる ret を返す
-pub fn rerooting_dp<T, F, G, H>(edge: &[(usize, usize)], id: T, f: F, g: G, h: H) -> Vec<T>
+pub fn rerooting_dp<T, U, F, G, H>(edge: &[(usize, usize)], id: U, f: F, g: G, h: H) -> Vec<T>
 where
     T: Clone + Default,
-    F: Fn(&T, &T) -> T,
-    G: Fn(&T, usize) -> T,
-    H: Fn(&T, usize) -> T,
+    U: Clone + Default,
+    F: Fn(&U, &U) -> U,
+    G: Fn(&T, usize) -> U,
+    H: Fn(U, usize) -> T,
 {
     let n = edge.len() + 1;
     let mut adj = vec![vec![]; n];
@@ -39,11 +40,11 @@ where
         for (ix, &j) in adj[i].iter().enumerate().filter(|&(_, &j)| j != parent[i]) {
             prod = f(&prod, &g(&dp[i][ix], j));
         }
-        dp[parent[i]][ix_for_parent[i]] = h(&prod, parent[i]);
+        dp[parent[i]][ix_for_parent[i]] = h(prod, parent[i]);
     }
     let mut ret = vec![T::default(); n];
     for &i in order.iter() {
-        let mut accum_back = vec![T::default(); adj[i].len()];
+        let mut accum_back = vec![U::default(); adj[i].len()];
         *accum_back.last_mut().unwrap() = id.clone();
         for j in (1..accum_back.len()).rev() {
             accum_back[j - 1] = f(&g(&dp[i][j], adj[i][j]), &accum_back[j]);
@@ -51,10 +52,10 @@ where
         let mut accum = id.clone();
         for (ix, &j) in adj[i].iter().enumerate() {
             let prod = f(&accum, &accum_back[ix]);
-            dp[j][ix_for_adj[i][ix]] = h(&prod, j);
+            dp[j][ix_for_adj[i][ix]] = h(prod, j);
             accum = f(&accum, &g(&dp[i][ix], j));
         }
-        ret[i] = h(&accum, i);
+        ret[i] = h(accum, i);
     }
     ret
 }
